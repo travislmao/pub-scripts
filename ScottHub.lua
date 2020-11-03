440a588f945380b759f2f2ea70ace839c4c16894
travislmao <73213159+travislmao@users.noreply.github.com>
2020-11-02 21:37:23 -0700

    Create ScottHub.lua

---
+++ ScottHub.lua
@@ -0,0 +1,16 @@
+if game.PlaceId == 3527629287 then 
+--paintball
+loadstring(game:HttpGet("https://raw.githubusercontent.com/travislmao/pub-scripts/main/paintball.lua", true))()
+elseif game.PlaceId == 1458767429 then
+--ABA
+loadstring(game:HttpGet("https://raw.githubusercontent.com/travislmao/pub-scripts/main/aba2gui.lua", true))()
+elseif game.PlaceId == 606849621 then
+--Jailbreak
+loadstring(game:HttpGet("https://raw.githubusercontent.com/travislmao/pub-scripts/main/JailbreakGUI.lua", true))()
+elseif game.PlaceId == 1962086868 then
+-- tower of hell
+loadstring(game:HttpGet("https://raw.githubusercontent.com/travislmao/pub-scripts/main/TowerOfHell.lua", true))()
+elseif game.PlaceId == 155615604 then
+-- prison life 2
+loadstring(game:HttpGet("https://raw.githubusercontent.com/travislmao/pub-scripts/main/prisonlife2.lua", true))()
+end
